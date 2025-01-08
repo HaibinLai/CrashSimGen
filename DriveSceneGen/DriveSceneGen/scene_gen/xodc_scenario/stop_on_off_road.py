af662f0c73d7ab55e6fa5daac27834c842435758
HaibinLai <12211612@mail.sustech.edu.cn>
2025-01-09 02:42:50 +0800

    Successfully gengerate xosc

--- DriveSceneGen/DriveSceneGen/scene_gen/xodc_scenario/stop_on_off_road.py
+++ DriveSceneGen/DriveSceneGen/scene_gen/xodc_scenario/stop_on_off_road.py
@@ -8,103 +8,182 @@
 
   Copyright (c) 2022 The scenariogeneration Authors.
 
-    Example showing how to create two separate roads (that are not linked), hence adjust_roads_and_lanes will not be able to set the geometries without a similar approach.
+    Simple example showing how one vehicle triggers based on the speed of another vehicle, then changes its speed
 
     Some features used:
 
-    - PlanView
+    - SpeedCondition
 
+    - AbsoluteSpeedAction
 
+    - RoadPosition
+
+    - OffroadCondition
 """
 
-from scenariogeneration import xodr, prettyprint, ScenarioGenerator
-import numpy as np
 import os
+from scenariogeneration import xosc, prettyprint, ScenarioGenerator
 
 
 class Scenario(ScenarioGenerator):
     def __init__(self):
         super().__init__()
+        self.open_scenario_version = 2
 
-    def road(self, **kwargs):
-        odr = xodr.OpenDrive("myroad")
+    def scenario(self, **kwargs):
+        ### create catalogs
+        catalog = xosc.Catalog()
+        catalog.add_catalog("VehicleCatalog", "../xosc/Catalogs/Vehicles")
 
-        # ---------------- Road 1
-        planview = xodr.PlanView(0, 0, 0)
+        ### create road
+        road = xosc.RoadNetwork(
+            roadfile="../xodr/e6mini.xodr", scenegraph="../models/e6mini.osgb"
+        )
 
-        # create some geometries and add to the planview
-        planview.add_geometry(xodr.Line(100))
+        ### create parameters
+        paramdec = xosc.ParameterDeclarations()
 
-        # create a solid roadmark
-        rm = xodr.RoadMark(xodr.RoadMarkType.solid, 0.2)
+        paramdec.add_parameter(
+            xosc.Parameter("$HostVehicle", xosc.ParameterType.string, "car_white")
+        )
+        paramdec.add_parameter(
+            xosc.Parameter("$TargetVehicle", xosc.ParameterType.string, "car_red")
+        )
 
-        # create centerlane
-        centerlane_1 = xodr.Lane(a=2)
-        centerlane_1.add_roadmark(rm)
-        lanesec_1 = xodr.LaneSection(0, centerlane_1)
+        ### create vehicles
 
-        # add a driving lane
-        lane2_1 = xodr.Lane(a=3.1)
-        lane2_1.add_roadmark(rm)
-        lanesec_1.add_left_lane(lane2_1)
+        bb = xosc.BoundingBox(2, 5, 1.8, 2.0, 0, 0.9)
+        fa = xosc.Axle(0.523598775598, 0.8, 1.68, 2.98, 0.4)
+        ba = xosc.Axle(0.523598775598, 0.8, 1.68, 0, 0.4)
+        white_veh = xosc.Vehicle(
+            "car_white", xosc.VehicleCategory.car, bb, fa, ba, 69, 10, 10
+        )
 
-        lane3_1 = xodr.Lane(a=3.1)
-        lane3_1.add_roadmark(rm)
-        lanesec_1.add_right_lane(lane3_1)
+        white_veh.add_property_file("../models/car_white.osgb")
+        white_veh.add_property("model_id", "0")
 
-        ## finalize the road
-        lanes_1 = xodr.Lanes()
-        lanes_1.add_lanesection(lanesec_1)
+        bb = xosc.BoundingBox(1.8, 4.5, 1.5, 1.3, 0, 0.8)
+        fa = xosc.Axle(0.523598775598, 0.8, 1.68, 2.98, 0.4)
+        ba = xosc.Axle(0.523598775598, 0.8, 1.68, 0, 0.4)
+        red_veh = xosc.Vehicle(
+            "car_red", xosc.VehicleCategory.car, bb, fa, ba, 69, 10, 10
+        )
 
-        road = xodr.Road(1, planview, lanes_1)
+        red_veh.add_property_file("../models/car_red.osgb")
+        red_veh.add_property("model_id", "2")
 
-        odr.add_road(road)
+        ## create entities
 
-        # ---------------- Road 2
+        egoname = "Ego"
+        targetname = "Target"
 
-        planview2 = xodr.PlanView(x_start=0, y_start=10, h_start=np.pi / 2)
-        # planview2 = xodr.PlanView()
+        entities = xosc.Entities()
+        entities.add_scenario_object(egoname, white_veh)
+        entities.add_scenario_object(targetname, red_veh)
 
-        # create some geometries and add to the planview
-        planview2.add_geometry(xodr.Line(200))
+        ### create init
 
-        # create a solid roadmark
-        rm = xodr.RoadMark(xodr.RoadMarkType.solid, 0.2)
+        init = xosc.Init()
+        step_time = xosc.TransitionDynamics(
+            xosc.DynamicsShapes.step, xosc.DynamicsDimension.time, 1
+        )
 
-        # create centerlane
-        centerlane = xodr.Lane(a=2)
-        centerlane.add_roadmark(rm)
-        lanesec = xodr.LaneSection(0, centerlane)
+        egospeed = xosc.AbsoluteSpeedAction(
+            25,
+            xosc.TransitionDynamics(
+                xosc.DynamicsShapes.sinusoidal, xosc.DynamicsDimension.time, 8
+            ),
+        )
+        egostart = xosc.TeleportAction(xosc.LanePosition(25, 0, -3, 0))
 
-        # add a driving lane
-        lane2 = xodr.Lane(a=3.1)
-        lane2.add_roadmark(rm)
-        lanesec.add_left_lane(lane2)
+        targetspeed = xosc.AbsoluteSpeedAction(15, step_time)
+        targetstart = xosc.TeleportAction(xosc.RoadPosition(30, -5, 0))
 
-        lane3 = xodr.Lane(a=3.1)
-        lane3.add_roadmark(rm)
-        lanesec.add_right_lane(lane3)
+        init.add_init_action(egoname, egospeed)
+        init.add_init_action(egoname, egostart)
+        init.add_init_action(targetname, targetspeed)
+        init.add_init_action(targetname, targetstart)
 
-        ## finalize the road
-        lanes = xodr.Lanes()
-        lanes.add_lanesection(lanesec)
+        ### create an event
 
-        road2 = xodr.Road(2, planview2, lanes)
+        trigcond = xosc.RelativeSpeedCondition(0, xosc.Rule.lessThan, egoname)
 
-        odr.add_road(road2)
+        trigger = xosc.EntityTrigger(
+            "mytesttrigger", 0.2, xosc.ConditionEdge.none, trigcond, targetname
+        )
 
-        # ------------------ Finalize
-        odr.adjust_roads_and_lanes()
+        event = xosc.Event("myfirstevent", xosc.Priority.overwrite)
+        event.add_trigger(trigger)
 
-        return odr
+        sin_time = xosc.TransitionDynamics(
+            xosc.DynamicsShapes.linear, xosc.DynamicsDimension.time, 5
+        )
+        action = xosc.RelativeLaneChangeAction(
+            -8,
+            targetname,
+            xosc.TransitionDynamics(
+                xosc.DynamicsShapes.linear, xosc.DynamicsDimension.time, 6
+            ),
+            -10,
+        )
+        event.add_action("newspeed", action)
+
+        ## create the maneuver
+        man = xosc.Maneuver("my_maneuver")
+        man.add_event(event)
+
+        mangr = xosc.ManeuverGroup("mangroup")
+        mangr.add_actor(targetname)
+        mangr.add_maneuver(man)
+        starttrigger = xosc.ValueTrigger(
+            "starttrigger",
+            0,
+            xosc.ConditionEdge.rising,
+            xosc.SimulationTimeCondition(0, xosc.Rule.greaterThan),
+        )
+        act = xosc.Act("my_act", starttrigger)
+        act.add_maneuver_group(mangr)
+
+        ## create the story
+        storyparam = xosc.ParameterDeclarations()
+        storyparam.add_parameter(
+            xosc.Parameter("$owner", xosc.ParameterType.string, targetname)
+        )
+        story = xosc.Story("mystory", storyparam)
+        story.add_act(act)
+
+        ## create the storyboard
+        stoptrigger = xosc.EntityTrigger(
+            "stop_simulation",
+            0.5,
+            xosc.ConditionEdge.rising,
+            xosc.OffroadCondition(0.5),
+            targetname,
+            triggeringpoint="stop",
+        )
+        sb = xosc.StoryBoard(init, stoptrigger)
+        sb.add_story(story)
+
+        ## create the scenario
+        sce = xosc.Scenario(
+            "adapt_speed_example",
+            "Mandolin",
+            paramdec,
+            entities=entities,
+            storyboard=sb,
+            roadnetwork=road,
+            catalog=catalog,
+            osc_minor_version=self.open_scenario_version,
+        )
+        return sce
 
 
 if __name__ == "__main__":
     sce = Scenario()
     # Print the resulting xml
-    prettyprint(sce.road().get_element())
+    prettyprint(sce.scenario().get_element())
 
-    # write the OpenDRIVE file as xosc using current script name
+    # write the OpenSCENARIO file as xosc using current script name
     sce.generate(".")
 
     # uncomment the following lines to display the scenario using esmini
